9a90b59a44cbddf8f44974ee23620708b9ed3b90
Ani <animuthu03@gmail.com>
2024-07-30 09:54:12 +0530

    Selenium project

---
+++ src/main/java/com/ani/test/AssessmentManualSeleApplication.java
@@ -0,0 +1,14 @@
+package com.ani.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentManualSeleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentManualSeleApplication.class, args);
+
+	}
+
+}
